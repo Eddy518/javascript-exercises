43c72af62f22bae22e5204f1700f01d7bbfe6cd3
Eddy518 <edmwangi2@gmail.com>
2023-07-24 16:26:11 +0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let withoutPunctuation = str.replace(/[\W_]/g, "");
 
+  withoutPunctuation = withoutPunctuation.toLowerCase();
+
+  let reverseString = withoutPunctuation.split("").reverse().join("");
+
+  reverseString = reverseString.toLowerCase();
+
+  if (withoutPunctuation === reverseString) {
+    return true;
+  } else {
+    return false;
+  }
 };
-
 // Do not edit below this line
 module.exports = palindromes;
