2a3135ccd2155adb604a0ae7c0d2aed8fd4e493f
Eddy518 <edmwangi2@gmail.com>
2023-07-25 08:42:23 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  if (num > 0) {
+    let fib = [1, 1];
+    for (let i = 2; i < num; i++) {
+      fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib[num - 1];
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
