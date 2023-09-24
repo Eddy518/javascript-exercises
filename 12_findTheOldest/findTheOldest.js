34a9e64d90cc84296f1a83011890812c8f204142
Eddy518 <edmwangi2@gmail.com>
2023-09-24 20:56:51 +0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,14 @@
-const findTheOldest = function() {
-
-};
-
+function findTheOldest(people) {
+  const currentYear = new Date().getFullYear();
+  const oldest = people
+    .map((person) => {
+      const age = person.yearOfDeath
+        ? person.yearOfDeath - person.yearOfBirth
+        : currentYear - person.yearOfBirth;
+      return { ...person, age: age };
+    })
+    .sort((a, b) => (a.age > b.age ? -1 : 1));
+  return oldest[0];
+}
 // Do not edit below this line
 module.exports = findTheOldest;
